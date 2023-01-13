10da6d05ddbe3a6840c00c01467983f7ef1c7b2f
adder013 <yagod99@gmail.com>
2023-01-13 23:08:27 +0500

    123

--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-from streamlit.ReportThread import get_report_ctx
 from functools import cache
 import streamlit as st
 from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
