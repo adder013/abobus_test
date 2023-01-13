64d80bc34d41b66f9244135084fd3e719c13bbed
adder013 <yagod99@gmail.com>
2023-01-13 22:53:32 +0500

    123

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from PIL import Image
 import numpy as np
+from model import Prediction, MODEL
 
 file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 
@@ -16,3 +17,7 @@ if file is not None:
     img_array = np.array(image)
     img = tf.image.resize(img_array, size=(224,224))
     img = tf.expand_dims(img, axis=0)
+
+if st.session_state.question and st.session_state.context:
+    answer_dict = (Prediction.get_model()(image=st.session_state.imag))
+    st.text_input('Ответ', value=answer_dict['answer'], disabled=True)
